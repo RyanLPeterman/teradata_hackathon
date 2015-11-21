e7b671f758c9ee02cb618a8ae777ea9e595cfe05
Ubuntu <ubuntu@ip-172-31-24-215.us-west-2.compute.internal>
2015-11-21 20:28:07 +0000

    launch

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello, World!"
+
+@app.route("/test")
+def test():
+    return render_template("test.html")
 
 if __name__ == "__main__":
     app.run()
