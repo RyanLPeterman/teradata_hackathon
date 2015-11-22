09f3eed167828c42fdf4c14ef0b67132c3e9e47c
Vic Yeh <vic317_yeh@hotmail.com>
2015-11-22 00:46:44 +0000

    weighted crime points added

--- backend/query.py
+++ backend/query.py
@@ -6,31 +6,31 @@ import sys
 import json
 
 def getrating(d):
-	if "traffic" in d:
+	if "mischief".upper() in d:
 		return 2
-	if "possession" in d:
-		return 2
-	if "theft" in d:
-		if "petty" in d:
-			return 3
-		else:
+	if "possession".upper() in d or "firearm".upper() in d:
+		return 3
+	if "theft".upper() in d or "burglary".upper() in d:
 			return 5
-	if "rape" in d or "abuse" in d:
+	if "rape".upper() in d or "abuse".upper() in d:
 		return 8
-	if "robbery" in d:
+	if "robbery".upper() in d:
 		return 6
-	if "auto" in d or "moto" in d:
+	if "auto".upper() in d or "moto".upper() in d or "truck".upper() in d:
 		return 4
-	if "battery" in d or "assault" in d:
+	if "battery".upper() in d or "assault".upper() in d or "violence".upper() in d or "injury".upper() in d:
 		return 7
-	
+	if "arson".upper() in d:
+		return 7
+	return 1
+		
 
 def getdata():
 	try:
 		con = mdb.connect('localhost', 'root', 'root', 'crime');
 
 		cur = con.cursor()
-		cur.execute("select x, y from crime;")
+		cur.execute("select x, y, description from crime;")
 		crime = cur.fetchall()
 		cur.execute("select count(*) from crime;")
 		count = len(crime)
@@ -41,13 +41,17 @@ def getdata():
 		sys.exit(1)
 		
 	finally:    
+		#print crime
 		data = []
 		for x in enumerate(crime):
 			temp = {}
-			temp['lat'] = x[1][0]
-			temp['lng'] = x[1][1]
+			temp['lat'] = x[1][1]
+			temp['lng'] = x[1][0]
+			temp['count'] = getrating(x[1][2])
 			data.append(temp)
 		return  json.dumps({'max' : count , 'data': data})
 
 		if con:    
 			con.close()
+
+print getdata()
